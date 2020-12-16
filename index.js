db10764c079024b64609ddb603e01c714cd1f570
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2020-12-16 13:29:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class Dog {
   if (this.sex === 'male') {
       return `It's me! ${this.name}, the parrot!`;
     } else {
-      return `${this.name} says Squawk!`;
+      return `${this.name} says squawk!`;
     }
   }
 }
